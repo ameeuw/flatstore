2fec94ec5efad30ef93f0f62eb3077eb3b804938
Arne Meeuw <arne.meeuw@gmail.com>
2018-11-15 02:13:57 +0100

    Changed to class structure

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 let fs = require('fs')
-let { join } = require('path')
 
-module.exports = function(opts={}) {
-  let file = opts.file || './store.json'
-  let template = opts.template || {}
+class Flatstore {
+  constructor (opts={}) {
+    this.file = opts.file || './store.json'
+    this.template = opts.template || {}
+  }
 
-  let appMethods = {
-    loadStore: function() {
-      try {
-        store = JSON.parse(fs.readFileSync(file).toString())
-        return store
-      } catch(err) {
-        console.log("No store found...")
-        return this.createStore()
-      }
-    },
-
-    saveStore: function(filename=file) {
-      return new Promise( (resolve, reject) => {
-        fs.writeFile(filename, JSON.stringify(store,null,2), function(err) {
-          if(err) {
-            console.log(err)
-            reject({'error': err})
-          }
-          console.log(`Written store to: ${filename}`)
-          resolve(store)
-        })
-      })
-    },
-
-    createStore: function() {
-      console.log("Creating new store...")
-      store = template
-      this.saveStore()
-      return store
+  loadStore () {
+    try {
+      this.store = JSON.parse(fs.readFileSync(file).toString())
+      return this.store
+    } catch(err) {
+      console.log("No store found...")
+      return this.createStore()
     }
   }
 
-  return appMethods
+  saveStore () {
+    return new Promise( (resolve, reject) => {
+      fs.writeFile(this.filename, JSON.stringify(this.store,null,2), function(err) {
+        if(err) {
+          console.log(err)
+          reject({'error': err})
+        }
+        console.log(`Written store to: ${this.filename}`)
+        resolve(this.store)
+      })
+    })
+  }
+
+  createStore () {
+    console.log("Creating new store...")
+    this.store = template
+    this.saveStore()
+    return this.store
+  }
+
+  get store () {
+    return this.store
+  }
 }
+
+module.exports = Flatstore
